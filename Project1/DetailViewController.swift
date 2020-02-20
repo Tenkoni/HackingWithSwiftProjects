78d3a96c7067aa59c27207f82582884b7e459752
Lu.Mi <caliminion@gmail.com>
2020-02-19 20:13:12 -0600

    Initial commit for the first project of Hacking with Swift, so far the table showing the image names has been implemented

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Lui on 19/02/20.
+//  Copyright © 2020 LuMi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
